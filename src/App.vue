d8a67f7db9a32ad0bf2d608794cf4dfbd74fafb5
Zheng Hao <zhengh513@outloook.com>
2024-02-07 13:18:30 +0800

    feat: 获取数据

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,131 @@
 <template>
   <div>
     <h3 class="title">{{ locationName }}</h3>
+    <realtime-weather
+      :text="weather?.text"
+      :temp="weather?.temp"
+      :category="air?.category"
+      :aqi="air?.aqi"
+    />
+    <forecast-everyday :everyday-list="everydayList" />
+    <forecast-hour :hour-list="hourList" />
   </div>
 </template>
 
 <script setup lang="ts">
+import {
+  AirQualityData,
+  DailyWeatherInfo,
+  HourlyWeatherInfo,
+  WeatherData,
+  WeatherLocation,
+} from '@/types'
+
+const key = '09de6ea036df4ce48519cc1689d522ab'
+
+const locationId = ref('')
 const locationName = ref('')
+const location = ref('')
+
+const weather = ref<WeatherData['now'] | null>(null)
+const air = ref<AirQualityData['now'] | null>(null)
+
+const getWeather = async () => {
+  const { data: data1 } = await axios.get<WeatherData>(
+    'https://devapi.qweather.com/v7/weather/now',
+    {
+      params: {
+        location: locationId.value,
+        key,
+      },
+    },
+  )
+  const { data: data2 } = await axios.get<AirQualityData>(
+    'https://devapi.qweather.com/v7/air/now',
+    {
+      params: {
+        location: locationId.value,
+        key,
+      },
+    },
+  )
+  weather.value = data1.now
+  air.value = data2.now
+}
+
+const everydayList = ref<DailyWeatherInfo['daily']>([])
+const hourList = ref<HourlyWeatherInfo['hourly']>([])
+const listEveryday = async () => {
+  const { data } = await axios.get<DailyWeatherInfo>(
+    'https://devapi.qweather.com/v7/weather/7d',
+    {
+      params: {
+        location: locationId.value,
+        key,
+      },
+    },
+  )
+  everydayList.value = data.daily
+}
+const listHour = async () => {
+  const { data } = await axios.get<HourlyWeatherInfo>(
+    'https://devapi.qweather.com/v7/weather/24h',
+    {
+      params: {
+        location: locationId.value,
+        key,
+      },
+    },
+  )
+  hourList.value = data.hourly
+}
+
+const getLocationName = async () => {
+  const { data } = await axios.get<WeatherLocation>(
+    'https://geoapi.qweather.com/v2/city/lookup',
+    {
+      params: {
+        location: location.value,
+        key,
+      },
+    },
+  )
+
+  locationName.value = data.location[0].name
+  locationId.value = data.location[0].id
+}
+
+const getLocation = () => {
+  navigator.geolocation.getCurrentPosition(
+    (pos) => {
+      const crd = pos.coords
+      location.value = `${crd.longitude.toFixed(2)},${crd.latitude.toFixed(2)}`
+    },
+    (err) => {
+      // eslint-disable-next-line no-console
+      console.warn(`ERROR(${err.code}): ${err.message}`)
+    },
+    {
+      enableHighAccuracy: true,
+      timeout: 5000,
+      maximumAge: 0,
+    },
+  )
+}
+
+onBeforeMount(() => {
+  getLocation()
+})
+
+watch(location, async () => {
+  await getLocationName()
+})
+
+watch(locationId, async () => {
+  await getWeather()
+  await listEveryday()
+  await listHour()
+})
 </script>
 
 <style scoped>
--- src/types/index.ts
+++ src/types/index.ts
@@ -67,3 +67,93 @@ export interface HourlyWeatherInfo {
   hourly: HourlyWeather[]
   refer: ReferInfo
 }
+
+// 实时天气
+export interface WeatherData {
+  code: string
+  updateTime: string
+  fxLink: string
+  now: NowData
+  refer: ReferInfo
+}
+
+interface NowData {
+  obsTime: string
+  temp: string
+  feelsLike: string
+  icon: string
+  text: string
+  wind360: string
+  windDir: string
+  windScale: string
+  windSpeed: string
+  humidity: string
+  precip: string
+  pressure: string
+  vis: string
+  cloud: string
+  dew: string
+}
+
+// 空气质量
+export interface AirQualityData {
+  code: string
+  updateTime: string
+  fxLink: string
+  now: NowData
+  station: StationData[]
+  refer: ReferInfo
+}
+
+interface NowData {
+  pubTime: string
+  aqi: string
+  level: string
+  category: string
+  primary: string
+  pm10: string
+  pm2p5: string
+  no2: string
+  so2: string
+  co: string
+  o3: string
+}
+
+interface StationData {
+  pubTime: string
+  name: string
+  id: string
+  aqi: string
+  level: string
+  category: string
+  primary: string
+  pm10: string
+  pm2p5: string
+  no2: string
+  so2: string
+  co: string
+  o3: string
+}
+
+// 城市搜索
+export interface WeatherLocation {
+  code: string
+  location: Location[]
+  refer: ReferInfo
+}
+
+interface Location {
+  name: string
+  id: string
+  lat: string
+  lon: string
+  adm2: string
+  adm1: string
+  country: string
+  tz: string
+  utcOffset: string
+  isDst: string
+  type: string
+  rank: string
+  fxLink: string
+}
